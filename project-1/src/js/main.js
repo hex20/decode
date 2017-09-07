b446f9a1f406b61f39acaf9dfc6ca4fb9ca23249
Angus McKay <mckayangus3@gmail.com>
2017-09-08 01:11:59 +1200

    Changes to layout styles and basic js

--- project-1/src/js/main.js
+++ project-1/src/js/main.js
@@ -1,1 +1,40 @@
-console.log('is working');
+
+var tasksColumns = [];
+
+$(document).ready(function() {
+  
+    getColumns();
+
+    countTasks(tasksColumns);
+  
+    dragula(tasksColumns);
+  
+    $('.pro-nav__menu-icon').click(function() {
+      toggleMenu();
+    });
+  
+});
+
+function countTasks(Columns) {
+  $(Columns).each(function (index, element) {
+    var amount = element.children().length;
+    element.siblings('.pro-task__number').text(amount + ' Tasks');
+  });
+}
+
+function toggleTabs() {
+
+}
+
+function toggleMenu() {
+  $('.pro-sidebar').toggleClass('pro-sidebar--closed');
+}
+
+function getColumns() {
+  $('.pro-task__tasks').each(function(index, element) {
+    console.log(element);
+    tasksColumns.push($(element));   
+  })
+
+  return true;
+}
