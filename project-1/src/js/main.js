b2f3617ad55f76532002ee9006337efcb0286764
Angus McKay <mckayangus3@gmail.com>
2017-09-13 18:21:35 +1200

    add user-select none and fix js

--- project-1/src/js/main.js
+++ project-1/src/js/main.js
@@ -1,13 +1,12 @@
-
-var tasksColumns = [];
-
 $(document).ready(function() {
-  
-    getColumns();
+
+    var tasksColumns = $('.pro-task__tasks').get();
 
     countTasks(tasksColumns);
   
-    dragula(tasksColumns);
+    dragula(tasksColumns).on('drop', function() {
+      countTasks(tasksColumns);
+    });
   
     $('.pro-nav__menu-icon').click(function() {
       toggleMenu();
@@ -15,26 +14,13 @@ $(document).ready(function() {
   
 });
 
-function countTasks(Columns) {
-  $(Columns).each(function (index, element) {
-    var amount = element.children().length;
-    element.siblings('.pro-task__number').text(amount + ' Tasks');
+function countTasks(columns) {
+  $(columns).each(function (index, element) {
+    var amount = $(element).children().length;
+    $(element).siblings('.pro-task__number').text(amount + ' Tasks');
   });
-}
-
-function toggleTabs() {
-
 }
 
 function toggleMenu() {
   $('.pro-sidebar').toggleClass('pro-sidebar--closed');
-}
-
-function getColumns() {
-  $('.pro-task__tasks').each(function(index, element) {
-    console.log(element);
-    tasksColumns.push($(element));   
-  })
-
-  return true;
 }
